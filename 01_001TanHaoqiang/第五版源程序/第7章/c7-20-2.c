1fb470bd0e6b922bf259e89a4034076ddec3168a
Jorgen7062 <jorgen7062@foxmail.com>
2020-06-13 22:38:37 +0800

    delete for empty the branch

--- 01_001TanHaoqiang/第五版源程序/第7章/c7-20-2.c
+++
@@ -1,7 +0,0 @@
-//file2.c
-#include<stdio.h>
-void enter_string(char str[80]) 
-{
-	printf("please input a str \n");
-  	gets(str);       
-}
--- 01_001TanHaoqiang/第五版源程序/第7章/c7-20-4.c
+++
@@ -1,6 +0,0 @@
-//file4.c
-#include<stdio.h>
-void print_string(char str[])
-{
-  printf("%s\n",str);
-}
