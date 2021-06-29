3666817449497f5789bb34578b49ab6188f9866d
jorgen7062 <jorgen7062@foxmail.com>
2021-06-29 10:30:27 +0800

    add  CTestCode/signedVSunsigned.c

---
+++ CTestCode/signedVSunsigned.c
@@ -0,0 +1,37 @@
+/*Head File*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*Function*/
+int main()
+{
+	
+	short s16_a = -18;
+	unsigned char u8_b = 0;
+	char s8_b = 0;
+	
+	s8_b = s16_a;
+	
+	u8_b = -18;
+	
+	if((char)u8_b == -18)
+	{
+		printf("u8_b == -18\n");
+	}
+	else
+	{	
+		printf("u8_b > -18\n");
+	}
+		
+	
+	
+	printf("s16_a = %d,u8_b  = %d,s8_b = %d\n",s16_a,u8_b,s8_b);	
+
+	
+	
+	printf("按任意键退出...\n");	
+	//getchar();
+
+	return 0;
+}
